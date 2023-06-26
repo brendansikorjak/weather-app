806c1873a86b7ea60b4052a92d9990c901dc2011
Brendan Sikorjak <brendansikorjak@Brendans-MBP.fios-router.home>
2023-06-26 13:58:21 -0400

    getWeather function

--- script.js
+++ script.js
@@ -14,5 +14,28 @@ function getCoordinates() {
     })
     .then(function (data) {
       console.log(data);
+      console.log(data[0].name);
+      console.log(data[0].lat);
+      console.log(data[0].lon);
+      getWeather(data);
+    });
+}
+
+function getWeather(data) {
+  var lat = data[0].lat;
+  var long = data[0].lon;
+
+  fetch(
+    "https://api.openweathermap.org/data/2.5/forecast?lat=" +
+      lat +
+      "&lon=" +
+      long +
+      "&appid=f665b091e1140e43864e309a7561e122"
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
     });
 }
